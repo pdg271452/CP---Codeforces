fe52fa8a435c61305b60939f072492f4f3f1bca7
Pranjal Gupta <108633275+pdg271452@users.noreply.github.com>
2026-08-09 20:23:24 +0530

    Add Three Numbers on the Blackboard [A] from Codeforces

--- 2256/A - Three Numbers on the Blackboard/solution.cpp
+++ 2256/A - Three Numbers on the Blackboard/solution.cpp
@@ -1,35 +1,36 @@
 #include <iostream>
 #include <algorithm>
- 
+ 
 using namespace std;
- 
+ 
 void solve() {
     long long a[3];
-    if (!(cin >> a[0] >> a[1] >> a[2])) return;
-    
-    // Sort so that x <= y <= z# pranjal
+    cin >> a[0] >> a[1] >> a[2];
     sort(a, a + 3);
     
     long long x = a[0];
     long long y = a[1];
     long long z = a[2];
     
-    // Minimum range is min(z - x, y) # pranjal
-    long long ans = min(z - x, y);
-    cout << ans << "
+    if (z > x + y) {
+        cout << y << "
 ";
+    } else {
+        cout << z - x << "
+";
+    }
 }
- 
+ 
 int main() {
-    // Fast I/O #pranjal
+    // Fast I/O
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
     int t;
-    if (cin >> t) {
-        while (t--) {
-            solve();
-        }
+    cin >> t;
+    while (t--) {
+        solve();
     }
+    
     return 0;
 }
