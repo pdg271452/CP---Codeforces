adefcce6512ee04842f25a8f6e848c3c4a355ad0
Pranjal Gupta <108633275+pdg271452@users.noreply.github.com>
2026-09-12 22:08:43 +0530

    Add Min Max Game [A] from Codeforces

---
+++ 2263/A - Min Max Game/solution.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        int ones = 0, zeros = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+ 
+            if (x == 1)
+                ones++;
+            else
+                zeros++;
+        }
+ 
+        if (ones >= zeros)
+            cout << "Bessie
+";
+        else
+            cout << "Elsie
+";
+    }
+ 
+    return 0;
+}
